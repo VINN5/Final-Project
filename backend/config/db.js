3120d4c85898432ccb0726334ab74225f0ec407c
VINN5 <vincentwambui4@gmail.com>
2025-09-08 08:19:54 +0300

    Update db.js configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,16 +8,20 @@ console.log("Database Config:", {
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD ? "********" : "NOT SET",
   port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL,
 });
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }, // ✅ Required for Render PostgreSQL
+  ssl:
+    process.env.DB_SSL === "true"
+      ? { rejectUnauthorized: false } // for production (Render/Heroku/etc.)
+      : false,                        // for local development
 });
-
 
 pool.connect((err) => {
   if (err) {
